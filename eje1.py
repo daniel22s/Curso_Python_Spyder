ed2750e982226dec01e61363c67fd7340c54916f
daniel22s <cruzdani543@gmail.com>
2025-01-26 12:42:36 -0500

    Realizando ejercicio 1

--- eje1.py
+++ eje1.py
@@ -1,1 +1,10 @@
-print("Hola")
+import pandas as pd
+import numpy as np
+
+datos = {'Nombre':['Leonardo' , 'Joaquin','Alex','Paula','Enrique','Jhon'],
+         'Materias':['Lenguaje','EDF','Matematicas','Quimica','Ingles','Historia'],
+         'Calificaciones':['18','20','13','19','16','17'],
+         'Deportes':['Futbol','Natacion','Tennis','Voley','Natacion','Balon Mano']}
+
+df = pd.DataFrame(datos)
+print(df)
