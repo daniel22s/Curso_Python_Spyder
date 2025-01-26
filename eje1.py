c55245a638d82bf28c05b8e5da6a5989b921e4ad
daniel22s <cruzdani543@gmail.com>
2025-01-26 15:03:38 -0500

    Usando np.nan

--- eje1.py
+++ eje1.py
@@ -9,3 +9,17 @@ datos = {'Nombre':['Leonardo' , 'Joaquin','Alex','Paula','Enrique','Jhon'],
 df = pd.DataFrame(datos)
 print(df)
 print('\n'*4)
+
+
+#DATOS NO VALIDOS
+datos2 = {'Nombre':['Leonardo' , 'Joaquin','N/A','Paula','Enrique','Jhon'],
+         'Materias':['Lenguaje','EDF','Matematicas','Quimica','Ingles','N/A'],
+         'Calificaciones':['18','20',np.nan,'19','16','17'],
+         'Deportes':['Futbol','Natacion','Tennis','Voley','N/A','Balon Mano']}
+
+df2 = pd.DataFrame(datos2)
+print(df2)
+print('\n'*1)
+
+print(df2.info())
+
